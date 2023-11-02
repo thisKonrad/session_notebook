6febb01fd8c91bc046dbb7db20c1df8b8b8356d2
this.Konrad <konradlebt@gmail.com>
2023-11-02 11:00:09 +0100

    save nexit session

---
+++ java-script-variables-numbers/main.js
@@ -0,0 +1,19 @@
+
+
+let myName = "Peter Johnson";
+
+const myObject = {
+    name: myName,
+    age: 38,
+    installed: true,
+}
+console.log("myObject", myObject);
+console.log('hello!')
+
+let sum = 0;
+
+function count(){
+    sum++
+    console.log(sum)
+}
+count();
